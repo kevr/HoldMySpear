d0e96b2106c9b5a1bfda3651ab6555819b70e652
Kevin Morris <kevr@0cost.org>
2023-10-09 05:54:33 -0700

    feat: stop click-to-move transfer of owned-by-other spears
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- HoldMySpear/Patches/Functionality.cs
+++ HoldMySpear/Patches/Functionality.cs
@@ -29,6 +29,27 @@ static public class DropCheck
         string playerName = Player.m_localPlayer.GetPlayerName();
         return customData.ContainsKey("owner") && customData["owner"] == playerName;
     }
+
+    static public string Owner(ItemDrop.ItemData item, bool enforceOwnership)
+    {
+        if (item.m_customData.ContainsKey("owner"))
+        {
+            string owner = item.m_customData["owner"];
+
+            if (enforceOwnership)
+            {
+                ref Player player = ref Player.m_localPlayer;
+                if (owner != player.GetPlayerName())
+                {
+                    player.Message(MessageHud.MessageType.Center,
+                        $"Get your own spear, this one is held by {owner}!");
+                }
+            }
+
+            return owner;
+        }
+        return null;
+    }
 }
 
 [HarmonyPatch(typeof(ItemDrop.ItemData), nameof(ItemDrop.ItemData.GetTooltip), typeof(ItemDrop.ItemData), typeof(int),
@@ -65,24 +86,14 @@ static class ItemDropPickup
         // A local reference to item's custom data.
         ref Dictionary<string, string> customData = ref __instance.m_itemData.m_customData;
 
-        bool isOwned = false;
-        string owner = "(NOT SET)";
-
-        if (customData.ContainsKey("owner"))
-        {
-            isOwned = true;
-            owner = customData["owner"];
-        }
+        string owner = DropCheck.Owner(__instance.m_itemData, true);
+        bool isOwned = owner != null;
 
         ref Player player = ref Player.m_localPlayer;
         string playerName = player.GetPlayerName();
 
-        if (isOwned && customData["owner"] != playerName)
-        {
-            player.Message(MessageHud.MessageType.Center,
-                $"Get your own spear, this one is held by {owner}!");
+        if (isOwned && owner != playerName)
             return false;
-        }
 
         customData["owner"] = playerName;
         owner = customData["owner"];
@@ -130,5 +141,20 @@ static class ItemDropAdd
     static bool Prefix(Inventory __instance, ItemDrop.ItemData item)
     {
         return Utility.Addable(item);
+    }
+}
+
+[HarmonyPatch(typeof(Inventory), nameof(Inventory.MoveItemToThis), typeof(Inventory), typeof(ItemDrop.ItemData), typeof(int), typeof(int), typeof(int))]
+static class InventoryMoveItemToThisXY
+{
+    static bool Prefix(Inventory fromInventory, ItemDrop.ItemData item, int amount, int x, int y)
+    {
+        bool toPlayer = !fromInventory.Equals(Player.m_localPlayer.GetInventory());
+        if (!toPlayer)
+            return true;
+
+        bool addable = Utility.Addable(item);
+        DropCheck.Owner(item, true);
+        return addable;
     }
 }
